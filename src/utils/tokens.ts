f071af5007c05e6b66d936674fa4d0f1e00ce5fc
Paweł Kopij <pawelrd@gmail.com>
2024-03-01 10:22:56 +0100

    add tokens

---
+++ src/utils/tokens.ts
@@ -0,0 +1,40 @@
+export const COLORS = {
+    primary: '#F3C58B',
+    primaryLight: '#FAE6CD',
+    primaryDark: '#624627',
+    secondary: '#DF8753',
+    secondaryLight: '#E39669',
+    secondaryDark: '#6F4329',
+    tertiary: '#2F3B2D',
+    tertiaryLight: '#51654d',
+    tertiaryDark: '#01333E',
+    text: '#B5AA94',
+    info: '#4BB1CF',
+    danger: '#ED4F32',
+    success: '#15CD72',
+    warning: '#fcecf4',
+    muted: '#ECECEC',
+};
+
+export const SPACINGS = {
+    xsmall: 4,
+    small: 8,
+    medium: 10,
+    big: 14,
+    large: 18,
+    xlarge: 20,
+}
+
+export const FONT_SIZES = {
+    small: 14,
+    medium: 16,
+    big: 20,
+    large: 24,
+}
+
+export const DIMENSIONS = {
+    small: 50,
+    medium: 75,
+    big: 100,
+    large: 150,
+}
