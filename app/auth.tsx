3d95e31ef8bc74f8033c36ad999a0756b9f83e57
Paweł Kopij <pawelrd@gmail.com>
2024-03-05 11:47:50 +0100

    adjust auth screen

--- app/auth.tsx
+++ app/auth.tsx
@@ -8,9 +8,15 @@ import { COLORS, SPACINGS } from '../src/utils/tokens';
 
 import { Templates } from '../src/types/template';
 import FormInput from '../src/components/form/FormInput';
+import { FormInputProps } from '../src/types/components/formInput';
+import { Form } from '../src/types/auth/form';
+import useForm from '../src/hooks/useForm';
+import { AuthIcon } from '../src/components/icon/Icon';
+import FormFooter from '../src/components/form/FormFooter';
 
 const AuthScreen = () => {
     const { template, setTemplate } = useTemplateContext();
+    const { handleChange, handleSubmit, handleClearForm, userData } = useForm();
 
     // TODO: Loading spinner
     if(!template) {
@@ -24,25 +30,50 @@ const AuthScreen = () => {
     }
 
     const switchAuthFormType = () => {
+        handleClearForm();
         setTemplate(template.name === Templates.Signin ? Templates.Signup : Templates.Signin);
+    }
+    
+    const renderForm = () => {
+        const { fields } = template;
+
+        // TODO: for isValid prop, validation has to be added.
+
+        return fields.map((field: FormInputProps) => {
+            return (
+                <FormInput
+                    value={userData[field.id as keyof Form]}
+                    errorMessage="This is Error placeholder message just to check potential length of the error message container" 
+                    key={field.id} 
+                    id={field.id} 
+                    isValid 
+                    onChange={handleChange} 
+                    placeholder={field.placeholder} 
+                    type={field.type} 
+                    variant={field.variant} 
+                    icon={field.icon} 
+                />
+            )
+        })
     }
     
     return (
         <View style={[styles.rootContainer]}>
             <ImageBackground {...imageProps} >
                 <View style={styles.innerContainer}>
-                    <Heading pl={5} size='2xl' color={COLORS.primary} maxWidth='2/3'>{template.header}</Heading>
-                    <View style={styles.formContainer}>
-                        {/* //TODO: Form goes here */}
-                        <FormInput errorMessage='ERRRROR' id='id' isValid onChange={() => {}} placeholder='placeholder id ' type='text' value='' variant='Filled' icon='people-sharp' />
-                        <FormInput errorMessage='ERRRROR' id='id' isValid onChange={() => {}} placeholder='placeholder id ' type='text' value='' variant='Outline' icon='people-sharp'/>
-                        <FormInput errorMessage='ERRRROR' id='id' isValid onChange={() => {}} placeholder='placeholder id ' type='text' value='' variant='Underline' />
+                    <View style={{gap: SPACINGS.medium}}>
+                        <Heading style={styles.header} size="2xl">{template.header}</Heading>
+                        <AuthIcon style={styles.icon} />
+                        <View style={styles.formContainer}> 
+                            {renderForm()}
+                            <FormFooter templateName={template.name} />
+                        </View>
                     </View>
                     <View>
                         <Button 
                             fullWidth 
                             label={template.ctaText} 
-                            onPress={() => {}} 
+                            onPress={handleSubmit} 
                             size='Medium' 
                             type='Secondary' 
                             variant='Filled' 
@@ -74,7 +105,7 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
     },
     innerContainer: {
-        height: '90%',
+        height: '95%',
         padding: SPACINGS.large,
         justifyContent: 'space-between'
     },
@@ -82,4 +113,13 @@ const styles = StyleSheet.create({
         width: '100%',
         gap: SPACINGS.xlarge + 5,
     },
+    header: {
+        alignSelf: "center", 
+        color: COLORS.tertiaryLight,
+    },
+    icon: {
+        alignSelf: 'center', 
+        width: 200, 
+        height: 200
+    }
 })
